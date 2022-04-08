b8c7085568bfb2842e58eacaa6ce1080e37f4c27
kushal hiremath <41748298+kushalthenomad@users.noreply.github.com>
2022-04-08 10:20:24 +0530

    Highest Marks

--- Program.cs
+++ Program.cs
@@ -4,21 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _1.Calculator
+namespace _2.HighestMarks
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter 1st number:");
-            int a=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter 2nd number:");
-            int b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Addition: {0}", a + b);
-            Console.WriteLine("Subtraction: {0}", a - b);
-            Console.WriteLine("Multiplication: {0}", a * b);
-            Console.WriteLine("Division: {0}", a / b);
-
+            int[] marks = new int[5];
+            int max=0;
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter average marks of {0} Student: ",i+1);
+                marks[i]=Convert.ToInt32(Console.ReadLine());
+                if (i == 0)
+                {
+                    max=marks[0];
+                }
+                else if (max<marks[i])
+                {
+                    max=marks[i];
+                }
+            }
+            Console.WriteLine("Highest marks obtained {0}",max);
         }
     }
 }
